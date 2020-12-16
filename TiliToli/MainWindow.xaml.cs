c3725d59cf185e8c168564a51cab29adda1b8238
Bencezs1 <zsbence2003@gmail.com>
2020-12-16 14:54:59 +0100

    keverő gomb készités

--- TiliToli/MainWindow.xaml.cs
+++ TiliToli/MainWindow.xaml.cs
@@ -31,7 +31,7 @@ namespace TiliToli
         {
             Button ezGomb = sender as Button;
             Button nullaGomb = (Button)FindName("Button0");
-            
+
             var fTav = Math.Abs(ezGomb.Margin.Top - nullaGomb.Margin.Top);
             var vTav = Math.Abs(ezGomb.Margin.Left - nullaGomb.Margin.Left);
 
@@ -39,7 +39,8 @@ namespace TiliToli
             int ezGombIndex = Array.IndexOf(allas, ezGombFelirat);
             int nullaGombIndex = Array.IndexOf(allas, 0);
 
-            if ((fTav == 100 && vTav == 0) || (vTav == 100 && fTav == 0)) {
+            if ((fTav == 100 && vTav == 0) || (vTav == 100 && fTav == 0))
+            {
                 var seged = ezGomb.Margin;
                 ezGomb.Margin = nullaGomb.Margin;
                 nullaGomb.Margin = seged;
@@ -52,15 +53,39 @@ namespace TiliToli
                 if (allas.SequenceEqual(kesz))
                 {
                     MessageBox.Show("Gratulálok ");
-                    lepesek = 0;
                 }
-                
+
             }
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             Button0.Visibility = Visibility.Hidden;
+            lepesek = 0;
+            Random r = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                Button ezGomb = (Button)FindName("Button" + Convert.ToString(r.Next(1, 9)));
+                Button nullaGomb = (Button)FindName("Button0");
+
+                var fTav = Math.Abs(ezGomb.Margin.Top - nullaGomb.Margin.Top);
+                var vTav = Math.Abs(ezGomb.Margin.Left - nullaGomb.Margin.Left);
+
+                int ezGombFelirat = int.Parse(ezGomb.Content.ToString());
+                int ezGombIndex = Array.IndexOf(allas, ezGombFelirat);
+                int nullaGombIndex = Array.IndexOf(allas, 0);
+
+                if ((fTav == 100 && vTav == 0) || (vTav == 100 && fTav == 0))
+                {
+                    var seged = ezGomb.Margin;
+                    ezGomb.Margin = nullaGomb.Margin;
+                    nullaGomb.Margin = seged;
+
+                    var seged2 = ezGombFelirat;
+                    allas[nullaGombIndex] = seged2;
+                    allas[ezGombIndex] = 0;
+                }
+            }
         }
     }
 }
